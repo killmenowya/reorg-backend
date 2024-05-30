0a81065dc50d256a58dff56fdb2524bf1af183ac
killmenowya <handthad@gmail.com>
2024-05-30 20:26:27 +0800

    new file:   .gitignore 	new file:   hs_err_pid33872.log 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   replay_pid33872.log 	new file:   src/main/java/com/reorg/course_details/CourseDetailsApplication.java 	new file:   src/main/java/com/reorg/course_details/models/Mod.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/reorg/course_details/CourseDetailsApplicationTests.java 	new file:   wrapper/maven-wrapper.properties

---
+++ src/main/java/com/reorg/course_details/CourseDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.reorg.course_details;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseDetailsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/reorg/course_details/models/Mod.java
@@ -0,0 +1,46 @@
+package com.reorg.course_details.models;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Access;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Mod_details")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Mod {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String acadYear; // AY22/23
+    private String moduleCode; // cs1101s
+    private String title; // computing methodlogy 1
+    private String description; // shitty mod
+    private int modCredit; // 4
+    private String department; // computing
+    private String faculty; // computing
+    private int[] workload; // [4,4,1,4,4]
+    private LocalDateTime currDate; // the date today??
+    private Boolean required; // user input req or not
+    private String preReq; // preReq Tree
+    private String coReq; // coreqs
+    private String preclusion; // preclusion
+    private DateTimeFormatter examDate; // yyyy-mm-ddThh:mm:ss:mssZ
+    private DateTimeFormatter examDuration; // in minutes eg 120min == 2 hr
+
+}
