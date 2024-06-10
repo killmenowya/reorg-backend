7d62bfdeca568121eb913c012e31beae0300a3f6
killmenowya <handthad@gmail.com>
2024-06-10 18:21:22 +0800

    modified:   .vscode/settings.json 	modified:   calendar/src/main/java/com/reorg/calendar/webConfig/WebConfig.java 	deleted:    course-details/hs_err_pid33872.log 	modified:   course-details/pom.xml 	modified:   discovery-service/src/main/java/com/reorg/discovery_service/DiscoveryServiceApplication.java 	modified:   discovery-service/src/main/resources/application.properties 	modified:   journal/pom.xml 	modified:   user-setting/pom.xml

--- discovery-service/src/main/java/com/reorg/discovery_service/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/reorg/discovery_service/DiscoveryServiceApplication.java
@@ -2,10 +2,11 @@ package com.reorg.discovery_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
