1ebb2451475177b2e43ba0186e84c84055f4797a
killmenowya <handthad@gmail.com>
2024-06-10 16:08:34 +0800

    modified:   user-setting/src/main/java/com/reorg/user_setting/webconfig/WebConfig.java

--- user-setting/src/main/java/com/reorg/user_setting/webconfig/WebConfig.java
+++ user-setting/src/main/java/com/reorg/user_setting/webconfig/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("http://localhost:3000");
             }
         };
